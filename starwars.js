19c2f76c1116aac0d1ac8f9c2bd838dbf5d373f7
Ana Victoy <anavictoy@yahoo.com.br>
2019-04-15 19:00:27 -0300

    Da água pro vinho
    audio ainda  não do jeito que eu quero

--- starwars.js
+++ starwars.js
@@ -5,7 +5,7 @@
 //  - Quando um filme for clicado, exibir sua introdução
 var audio = new Audio("musica.ogg");
 var ultimoFilme = localStorage.getItem('ultimoFilme');
-if(ultimoFilme==null)ultimoFilme='4/';
+if(ultimoFilme==null) ultimoFilme='4/';
 pegaFilme(ultimoFilme);
 
 
@@ -13,34 +13,16 @@ function playAudio() {
     audio.currentTime = 0;
     audio.play();
 } 
-$("#epi1").click(function(){
-    link="4/";
-    pegaFilme(link);
-});
-$("#epi2").click(function(){
-    link="5/";
-    pegaFilme(link);
-});
-$("#epi3").click(function(){
-    link="6/";
-    pegaFilme(link);
-});
-$("#epi4").click(function(){
-    link="1/";
-    pegaFilme(link);
-});
-$("#epi5").click(function(){
-    link="2/";
-    pegaFilme(link);
-});
-$("#epi6").click(function(){
-    link="3/";
-    pegaFilme(link);
-});
-$("#epi7").click(function(){
-    link="7/";
-    pegaFilme(link);
-});
+
+let botoes=document.getElementsByClassName("episodio");
+for (i = 0; i < botoes.length; i++) {
+    botaoAtual = botoes[i];
+    botoes[i].addEventListener("click", function (botaoAtual) {
+        var link=botaoAtual.path[0].getAttribute('data-link');
+        pegaFilme(link);
+    }, false);
+}
+
 function pegaFilme(link){
     $(".reading-animation")[0].innerText="";
     playAudio();
@@ -49,14 +31,12 @@ function pegaFilme(link){
         numero="I";
         if(result.episode_id==2) numero+="I";
         else if(result.episode_id==3) numero+="II";
-        else if(result.episode_id==4)   numero="IV";
+        else if(result.episode_id==4) numero="IV";
         else if(result.episode_id==5) numero="V";
         else if(result.episode_id==6) numero="VI";
         else if(result.episode_id==7) numero="VII";
-
+        
         titulo= "Episode "+numero+"\n" +result.title.toUpperCase();
         $(".reading-animation")[0].innerText=titulo+"\n\n"+result.opening_crawl;
-
     }});
-
 }
